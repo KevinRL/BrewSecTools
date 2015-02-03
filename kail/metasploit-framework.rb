18ecb8782cd0c408e515238ad98149f51ce13128
KevinRL <yuanliu@L-118899-P13.symc.symantec.com>
2015-02-02 18:08:17 -0800

    remove comments

--- kail/metasploit-framework.rb
+++ kail/metasploit-framework.rb
@@ -1,3 +1,4 @@
+# author kevinl
 class MetasploitFramework < Formula
   homepage "http://metasploit.com/"
   url "https://github.com/rapid7/metasploit-framework/archive/HEAD.zip"
@@ -17,26 +18,7 @@ class MetasploitFramework < Formula
       bin.install_symlink Dir[prefix/"msf*"]
   end
 
-  test do
-    system "true"
-  end
-
   def post_install
-      #database setup
-      #system "initdb", "/usr/local/var/postgres"
-      #
-      #pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start 
-      #mkdir -p ~/Library/LaunchAgents
-      #ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
-      #sleep 5
-      #psql postgres -c "create role msf login password '$MSFPASS'" >> $LOGFILE 2>&1
-      #createdb -O msf msf -h localhost >> $LOGFILE 2>&1
-      #bundle
-      # brew install homebrew/versions/ruby193
-      #gem install pg
-      #gem install bundler
-      #gem install i18n
-      #gem install multi_json
       system('mkdir', '-p', File.join(prefix, '.git'))
   end
 
